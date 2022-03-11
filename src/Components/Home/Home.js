4d4207b8cba290994a834775a7974043e86198d9
kyle <phantom-kilo@live.com>
2022-03-10 20:15:04 -0700

    Added text to gunnery and home pages

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,3 +1,4 @@
+import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/row";
 import Col from "react-bootstrap/Col";
@@ -8,10 +9,31 @@ const Home = () => {
     <Container>
       <Row>
         <Col>
-          <h1>Tank API</h1>
+          <Card>
+            <Card.Title>The Why Behind the Project</Card.Title>
+            <Card.Body>
+              
+            </Card.Body>
+          </Card>
         </Col>
         <Col>
-          <h2>And General Tank Info</h2>
+          <Card>
+            <Card.Tite>*** Disclaimer ***</Card.Tite>
+            <Card.Body>
+              Every bit of info, all the vehicle stats, numbers, "official"
+              release documentation, is the unclassified version. Many of the
+              modern vehicles to be added are technically in service, but
+              finding info on how many or where is difficult. I've been in units
+              with the most up to date equipment, only to have it taken and sent
+              somewhere else, leaving us with older hardware. Foreign hardware
+              is even harder to track down, because my info is based on what is
+              publicly available. That means, like with the Russian military,
+              certain modern hardware might exist only on paper or a parade
+              ground. The Russian invasion of Ukraine in early 2022 only
+              comfirmed that most of what is available is inaccurate, outdated
+              or outright propaganda.
+            </Card.Body>
+          </Card>
         </Col>
       </Row>
     </Container>
--- src/Components/TankStuff/SubTankStuff/Gunnery.js
+++ src/Components/TankStuff/SubTankStuff/Gunnery.js
@@ -2,20 +2,46 @@ import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import "../TankStuff.css";
 
 const Gunnery = () => {
   return (
     <Container>
       <Row>
         <Col>
-          <Card>
+          <Card bg="black">
             <Card.Title>Gunnery Terms</Card.Title>
             <Card.Body>
               Operating any armored vehicle as a crew requires a different way
               of speaking. Military radio speak is direct and eliminates all
               unnecessary words, so as to save time. While important across the
               military, armor crews need to be extremely fluid and proficient
-              due to working together to operate a single weapon system.
+              due to working together to operate a single weapon system. One of
+              the most important conversations to be had is known as a Fire
+              Command. Fire commands get the whole crew on the same page with
+              just a few words, streamlining the process to get fire on target
+              as quickly as possible.
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card bg="black">
+            <Card.Title>Fire Command Structure</Card.Title>
+            <Card.Body>
+              <ul>
+                <li>Alert: Crew member called, "Gunner"</li>
+                <li>Ammo or Weapon: Ammo or weapon type, "Sabot"</li>
+                <li>Target: Target description, "Tank"</li>
+                <li>Range: Range to target in meters, "1200M"</li>
+                <li>Direction: Direction of target, "12 o'clock"</li>
+                <li>
+                  Execution: Fire command, sometimes also change ammo type,
+                  "Fire"
+                </li>
+                <li>Termination: Stop firing, "Target, cease fire"</li>
+              </ul>
+              Together, this looks like "Gunner, Sabot, Tank, 1200m, 12 o'clock,
+              Fire, Target, Cease Fire"
             </Card.Body>
           </Card>
         </Col>
@@ -23,18 +49,20 @@ const Gunnery = () => {
       <Row>
         <Col>
           <Card bg="black">
-            <Card.Title>General Army Jargon</Card.Title>
+            <Card.Title>Fire Command Variations</Card.Title>
             <Card.Body>
-              <ul></ul>
+              Fire commands aren't always straight forward due to other
+              militaries not always playing along. Say, for instance, that the
+              main gun has the incorrect ammo type loaded. Because the loaded
+              round is fired regardless, both the loader and gunner need to know
+              to follow up with the correct round
             </Card.Body>
           </Card>
         </Col>
         <Col>
           <Card bg="black">
-            <Card.Title>Armor Specific Jargon</Card.Title>
-            <Card.Body>
-              <ul></ul>
-            </Card.Body>
+            <Card.Title>Other Tank Terms</Card.Title>
+            <Card.Body></Card.Body>
           </Card>
         </Col>
       </Row>
