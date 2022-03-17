039b07c7c7585e07e7cfa83edda6f7a752c9d63c
kyle <phantom-kilo@live.com>
2022-03-17 00:09:41 -0700

    Changed tank detail page layout to display all info

--- src/Components/TankDetails/TankDetails.js
+++ src/Components/TankDetails/TankDetails.js
@@ -2,6 +2,7 @@ import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+import Stack from "react-bootstrap/Stack";
 import "./TankDetails.css";
 
 const TankDetails = (props) => {
@@ -10,7 +11,7 @@ const TankDetails = (props) => {
 
   const images = tank.images.map((image) => {
     return (
-      <Col sm={6} md={4} lg={3} key={image._id}>
+      <Col sm={6} md={4} key={image._id}>
         <Card border="success" bg="black">
           <Card.Title>{image.description}</Card.Title>
           <Card.Img variant="top" className="img-fluid" src={image.url} />
@@ -19,17 +20,64 @@ const TankDetails = (props) => {
     );
   });
 
+  const rounds = tank.ammo.map((round) => {
+    return (
+      <Col sm={6}>
+        <Card border="success" bg="black">
+          <Card.Text>
+            <ul>
+              <li>Designation: {round.designation}</li>
+              <li>Role: {round.type}</li>
+              <li>Weight: {round.weight} KGs</li>
+            </ul>
+          </Card.Text>
+        </Card>
+      </Col>
+    );
+  });
+
+  const weapons = tank.secondary.map((weapon) => {
+    return (
+      <Card.Text>
+        <ul>
+          <li>Weapon: {weapon.weapon}</li>
+          <li>Ammo: {weapon.ammo}</li>
+        </ul>
+      </Card.Text>
+    );
+  });
+
   return (
     <Container>
-      <Row>
-        <Col>
-          <h1>{tank.model}</h1>
-        </Col>
-        <Col>
-          <h2>{tank.main}</h2>
-        </Col>
-      </Row>
-      <Row>{images}</Row>
+      <Stack gap={5}>
+        <Row>
+          <Col>
+            <h1>AFV: {tank.model}</h1>
+            <br />
+            <h2>Country: {tank.country}</h2>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Row>
+              <Col sm={12} md={8}>
+                <Card border="success" bg="black">
+                  <Card.Title>Main weapon: {tank.main}</Card.Title>
+                  <Card.Title>Ammo Types</Card.Title>
+                  <Row>{rounds}</Row>
+                </Card>
+              </Col>
+              <Col sm={12} md={4}>
+                <Card border="success" bg="black">
+                  <Card.Title>Other Weapons</Card.Title>
+                  <Card.Text>{weapons}</Card.Text>
+                </Card>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+        <Row>{images}</Row>
+      </Stack>
     </Container>
   );
 };
